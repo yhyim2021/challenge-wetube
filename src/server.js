68434aae82a0a4b08f6abd344b21fb83c4bef404
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-07 19:38:18 +0900

    connect-mongo 설치 및 적용

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import { localsMiddleware } from "./localsMiddlewares";
 
@@ -17,6 +18,9 @@ app.use(
     secret: "Hello!",
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/wetube",
+    }),
   })
 );
 
