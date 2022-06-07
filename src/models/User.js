34a8daf088dfec49c823985419caa5d162c84045
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-07 18:35:20 +0900

    bcrypt 설치 및 password hash화

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
+});
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const userModel = mongoose.model("User", userSchema);
