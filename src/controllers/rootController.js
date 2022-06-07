2686b716182d705608ed44f98161e4881264eacb
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-07 17:45:28 +0900

    join template

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,7 +1,9 @@
 export const home = (req, res) => {
   return res.render("home");
 };
-export const getJoin = (req, res) => {};
+export const getJoin = (req, res) => {
+  return res.render("join");
+};
 export const postJoin = (req, res) => {};
 export const getLogin = (req, res) => {};
 export const postLogin = (req, res) => {};
