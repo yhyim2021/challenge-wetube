f7e3837a25eeb40e8f716e3eb4ea2ad0a0b670b1
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-07 18:27:36 +0900

    user 정보에 대해서 db에 저장성공

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,14 +1,21 @@
+import User from "../models/User";
+
 export const home = (req, res) => {
   return res.render("home");
 };
 export const getJoin = (req, res) => {
   return res.render("join");
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   console.log(req.body);
   const {
     body: { email, username, password, cpassword },
   } = req;
+
+  if (password !== cpassword) {
+    return res.status(406).render("join");
+  }
+  const createUser = await User.create({ email, username, password });
   return res.redirect("/");
 };
 export const getLogin = (req, res) => {};
