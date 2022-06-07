048f0d19e4cfb0f4ae7352c2c27025aafed39e92
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-07 17:27:26 +0900

    pug 설치 및 환경설정 및 router 설정

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 import express from "express";
+import rootRouter from "./routers/rootRouter";
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.use("/", rootRouter);
 
 export default app;
