2939318867eb7af6f44537df2ee809bf27f92503
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-08 20:30:25 +0900

    uploads폴더를 express.static으로 등록

--- src/server.js
+++ src/server.js
@@ -38,5 +38,6 @@ app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/uploads", express.static("uploads"));
 
 export default app;
