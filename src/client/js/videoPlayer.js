aa360fadec62a0383a8d36f72fb9cd35d6e0c779
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-11 18:43:06 +0900

    playbtn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,16 @@
-console.log("video player");
+console.log("Video Player");
+
+const video = document.getElementById("video");
+const playBtn = document.getElementById("playBtn");
+const playBtnIcon = playBtn.querySelector("i");
+
+const handlePlayBtnClick = () => {
+  if (video.paused) {
+    video.play();
+    playBtnIcon.classList = "fas fa-play";
+  } else {
+    video.pause();
+    playBtnIcon.classList = "fas fa-pause";
+  }
+};
+playBtn.addEventListener("click", handlePlayBtnClick);
