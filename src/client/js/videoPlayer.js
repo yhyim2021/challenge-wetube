76086750c3774273f62e6615cb2cc2abcb99c85f
yhyim2021 <89031110+yhyim2021@users.noreply.github.com>
2022-06-11 18:54:47 +0900

    muteBtn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,8 @@ console.log("Video Player");
 const video = document.getElementById("video");
 const playBtn = document.getElementById("playBtn");
 const playBtnIcon = playBtn.querySelector("i");
+const muteBtn = document.getElementById("muteBtn");
+const muteBtnIcon = muteBtn.querySelector("i");
 
 const handlePlayBtnClick = () => {
   if (video.paused) {
@@ -13,4 +15,18 @@ const handlePlayBtnClick = () => {
     playBtnIcon.classList = "fas fa-pause";
   }
 };
+
+const handleMuteBtnClick = () => {
+  if (video.muted) {
+    video.muted = false;
+    muteBtnIcon.classList = "fas fa-volume-up";
+    console.log(video.muted);
+  } else {
+    video.muted = true;
+    muteBtnIcon.classList = "fas fa-volume-mute";
+    console.log(video.muted);
+  }
+};
+
 playBtn.addEventListener("click", handlePlayBtnClick);
+muteBtn.addEventListener("click", handleMuteBtnClick);
